6d3376c35e6d7e1211e186d6b2d57448e5d03692
anshy0304 <anshy030405@gmail.com>
2026-04-14 13:52:55 +0530

    feat: setup EF Core DbContext and JobRepository

---
+++ JobNexus/JobNexus.Storage/JobDbContext.cs
@@ -0,0 +1,18 @@
+﻿using JobNexus.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobNexus.Storage
+{
+    public class JobDbContext : DbContext
+    {
+        public JobDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BackgroundJob> Jobs { get; set; }
+
+    }
+}
---
+++ JobNexus/JobNexus.Storage/JobRepository.cs
@@ -0,0 +1,29 @@
+﻿using JobNexus.Core.Entities;
+using JobNexus.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobNexus.Storage
+{
+    public class JobRepository : IJobRepository
+    {
+        private readonly JobDbContext _context;
+        public JobRepository(JobDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<BackgroundJob?> GetNextPendingJobAsync()
+        {
+            return await _context.Jobs
+                .FirstOrDefaultAsync(j => j.Status == Core.Enums.JobStatus.Pending);
+        }
+        public async Task UpdateJobAsync(BackgroundJob job)
+        {
+            _context.Jobs.Update(job);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
