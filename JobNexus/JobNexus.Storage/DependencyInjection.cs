59b7acc3abfb07758c76662c658b805acd9ee0ae
anshy0304 <anshy030405@gmail.com>
2026-04-14 14:26:46 +0530

    feat :add initial ef core migration and generate database

--- JobNexus/JobNexus.Storage/DependencyInjection.cs
+++ JobNexus/JobNexus.Storage/DependencyInjection.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using JobNexus.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace JobNexus.Storage
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddStorage(this IServiceCollection services,string connectionString)
+        {
+            services.AddDbContext<JobDbContext>(options =>
+                 options.UseSqlServer(connectionString));
+
+            services.AddScoped<IJobRepository, JobRepository>();
+
+            return services;
+        }
     }
 }
---
+++ JobNexus/JobNexus.Storage/JobDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace JobNexus.Storage
+{
+    public class JobDbContextFactory : IDesignTimeDbContextFactory<JobDbContext>
+    {
+        public JobDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<JobDbContext>();
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=JobNexusDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            return new JobDbContext(optionsBuilder.Options);
+        }
+    }
+}
